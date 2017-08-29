24a39ae349ff8069a881d14b44beffe2c3669898
James Yorston <jamesyorston1994@gmail.com>
2017-08-29 15:52:10 +0000

    further config

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,10 @@
 class Pin < ActiveRecord::Base
     belongs_to :user
-    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+    has_attached_file :image,
+    :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+    :url =>':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename'
+    
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
 
